5261b266dd96ac2472d6023169974d85ecc23ce7
Juan Gerardo Medellin Ibarra <thegera4@hotmail.com>
2026-01-16 17:08:56 -0600

    update: fix xss in email

--- lib/email.ts
+++ lib/email.ts
@@ -9,16 +9,6 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-function escapeHtml(text: string | undefined | null): string {
-  if (!text) return '';
-  return text
-    .replace(/&/g, "&amp;")
-    .replace(/</g, "&lt;")
-    .replace(/>/g, "&gt;")
-    .replace(/"/g, "&quot;")
-    .replace(/'/g, "&#039;");
-}
-
 interface OrderEmailDetails {
   customerName: string;
   orderNumber?: string;
